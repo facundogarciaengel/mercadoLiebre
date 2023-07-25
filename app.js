e161fbc41c786ee539ce1c1467f70aa96926a26d
facundogarciaengel <fgarciaengel@gmail.com>
2023-07-25 20:05:58 -0300

    cambio listen

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const app = express()
 const path = require("path")
 const localPath = (path.join(__dirname, "public"))
 app.use(express.static(localPath))
-app.listen(3000, ()=>{
-    console.log("Escuchando en el puerto 3000")
-})
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Servidor corriendo");
+  })
 
 let rutaHome = path.join(__dirname, "/views/home.html")
 app.get("/", (req,res)=>{
